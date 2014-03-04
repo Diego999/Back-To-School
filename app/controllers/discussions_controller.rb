91724ea61d69fd72dfb05942de7cb8c7774eeb7a
David Kuhner <david.kuhner@he-arc.ch>
2014-03-04 15:59:48 +0100

    Add DiscussionController#index logic
    - Selection of all current_user related discussions
    - Raw output on discussions/index page

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -2,15 +2,15 @@ class DiscussionsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @discussions = current_user.discussions
+
+    current_user.promotions.each do |promotion|
+      @discussions += promotion.discussions
+      @discussions += promotion.establishment.discussions
+    end
+
+    @discussions.uniq #TODO : Need to be tested
 
   end
 
-  def show
-
-  end
-
-  def create
-
-  end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ BackToSchool::Application.routes.draw do
 
   root :to => "home#index"
 
+  resources :discussions
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
