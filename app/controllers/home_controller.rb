6a1418d84d36e95b1bdb5d12bfe524f9f5804948
Diego Antognini <diegoantognini@gmail.com>
2014-03-05 21:14:14 +0100

    Update side bar at the home page (without any interactions)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,15 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    promotions = current_user.promotions
+
+    followed_promotions = []
+    current_user.followers.each do |follow|
+      followed_promotions.append(follow.promotion)
+    end
+    promotion_students = promotions[0].students
+
+    promotion_followers = promotions[0].followers
+    @data = {:promotions => promotions, :followed_promotions => followed_promotions, :promotion_students => promotion_students, :promotion_followers => promotion_followers}
   end
 end
