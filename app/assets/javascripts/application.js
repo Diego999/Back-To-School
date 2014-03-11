268fddadb7d40e448536fb42d036733f6055a10c
Marco Aeberli <marco.aeberli@he-arc.ch>
2014-03-11 21:16:17 +0100

    Implementation of hiding flash boxes.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+
+$( document ).ready(function() {
+    /*
+     * Removes the flash message
+     */
+    $(".flashbox .close").click(function(){
+        $(this).parent().slideUp();
+    });
+});
