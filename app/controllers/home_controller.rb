a5c523960e595666974e663a13a45214b0cd9d6d
Diego Antognini <diegoantognini@gmail.com>
2014-03-13 15:04:38 +0100

    Refactor @data -> several @xxx

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,14 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    promotions = current_user.promotions
+    @promotions = current_user.promotions
 
-    followed_promotions = []
+    @followed_promotions = []
     current_user.followers.each do |follow|
-      followed_promotions.append(follow.promotion)
+      @followed_promotions.append(follow.promotion)
     end
-    promotion_students = promotions[0].students
+    @promotion_students = @promotions[0].students
 
-    promotion_followers = promotions[0].followers
-    @data = {:promotions => promotions, :followed_promotions => followed_promotions, :promotion_students => promotion_students, :promotion_followers => promotion_followers}
+    @promotion_followers = @promotions[0].followers
   end
 end
