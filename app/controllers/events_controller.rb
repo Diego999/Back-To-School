3d2065dd6b3d18d51f02937fc472975736d3194d
David Kuhner <david.kuhner@he-arc.ch>
2014-03-18 17:41:08 +0100

    Add: create method in events_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,12 @@ class EventsController < ApplicationController
   def index
     @events = current_user.get_all_events.uniq
   end
+
+  def create
+    @discussion = Discussion.find(params[:discussion_id])
+    @event = @discussion.events.create(params[:event].permit(:name,:location,:date))
+    @event.user = current_user
+    @event.save
+    redirect_to discussion_path(@discussion)
+  end
 end
