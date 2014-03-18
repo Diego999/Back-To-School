fd32e30984cd8e8e0ca64579121c385ff7e55306
David Kuhner <david.kuhner@he-arc.ch>
2014-03-18 17:39:48 +0100

    Add: create method in messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,11 @@
 class MessagesController < ApplicationController
+  before_filter :authenticate_user!
+
+  def create
+    @discussion = Discussion.find(params[:discussion_id])
+    @message = @discussion.messages.create(params[:message].permit(:message))
+    @message.user = current_user
+    @message.save
+    redirect_to discussion_path(@discussion)
+  end
 end
