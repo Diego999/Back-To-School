ae6bda478d464a67c03a30791f535da456ee05b9
Diego Antognini <diegoantognini@gmail.com>
2014-03-21 21:43:55 +0100

    Don't display followed promotion if we haven't been accepted

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -9,7 +9,9 @@ class DiscussionsController < ApplicationController
       @discussions += promotion.establishment.discussions
     end
     current_user.followers.each do |f|
-      @discussions += f.promotion.discussions
+      if f.accepted
+        @discussions += f.promotion.discussions
+      end
     end
     @discussion = Discussion.new
     @discussions.uniq #TODO : Need to be tested
