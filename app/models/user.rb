acbeb5914e59bde280254add3e6c4ba5cb190f01
Marco Aeberli <marco.aeberli@he-arc.ch>
2014-03-21 14:37:39 +0100

    ADD: Implementations to get the discussions of a user and also method to check if a user is participant of one of them.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  include Authority::UserAbilities
+
   has_and_belongs_to_many :promotions
   belongs_to :establishment
   has_many :messages
@@ -19,7 +21,7 @@ class User < ActiveRecord::Base
   def get_complete_name
     firstname + " " + lastname
   end
-
+  
   def get_all_discussions
     output = get_discussion_private + get_discussion_establishment
     get_discussion_promotion(output)
@@ -46,6 +48,28 @@ class User < ActiveRecord::Base
 
   def get_discussion_establishment
     get_default_promotion.establishment.get_discussion
+  end
+
+  def accepted_followed_promotions
+    result = Promotion.joins("INNER JOIN followers ON promotions.id = followers.promotion_id AND followers.accepted = true").where("followers.user_id = %s", self.id)
+  end
+
+  def accepted_followed_discussions
+    result = Discussion.joins("INNER JOIN discussions_promotions ON discussions.id = discussions_promotions.discussion_id") \
+                       .joins("INNER JOIN promotions ON discussions_promotions.promotion_id = promotions.id") \
+                       .joins("INNER JOIN followers ON promotions.id = followers.promotion_id AND followers.accepted = true") \
+                       .where("followers.user_id = %s", self.id)
+  end
+
+  def promotions_discussions
+    Discussion.joins("INNER JOIN discussions_promotions ON discussions.id = discussions_promotions.discussion_id") \
+              .joins("INNER JOIN promotions ON discussions_promotions.promotion_id = promotions.id") \
+              .joins("INNER JOIN promotions_users ON promotions.id = promotions_users.promotion_id") \
+              .where("promotions_users.user_id = %s", self.id)
+  end
+
+  def discussion_participant?(discussion)
+    discussions.include?(discussion) || promotions_discussions.include?(discussion) || accepted_followed_discussions.include?(discussion)
   end
 
 end
