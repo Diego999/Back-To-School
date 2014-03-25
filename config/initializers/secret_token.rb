14884e839429750c3de307a32c4f8d0d43545b01
David Kuhner <david.kuhner@he-arc.ch>
2014-03-25 17:55:04 +0100

    Fix: remove secret_token from gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,5 @@
+# This works but it's not secure ! ( kept there for study purpose )
+# BackToSchool::Application.config.secret_key_base = '1c3839b3be3aae73037e42f3ea0be34312c1faa139f0532ff29dda45863fe878506a8d01ca0690c78ecdfa8fe786320a3386168d8cd65211ebb9437085b270c2'
+
+# This is much more secure ! It uses the token loaded before.
+BackToSchool::Application.config.secret_key = APP_CONFIG[:secret_token]
