a4a6032fe97005f8bc48afefec99c277d225e05b
Marco Aeberli <marco.aeberli@he-arc.ch>
2014-03-25 07:43:20 +0100

    FIX: Unique discussion display. #26

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -2,19 +2,19 @@ class DiscussionsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @discussions = current_user.discussions
+    discussions = current_user.discussions
 
     current_user.promotions.each do |promotion|
-      @discussions += promotion.discussions
-      @discussions += promotion.establishment.discussions
+      discussions += promotion.discussions
+      discussions += promotion.establishment.discussions
     end
     current_user.followers.each do |f|
       if f.accepted
-        @discussions += f.promotion.discussions
+        discussions += f.promotion.discussions
       end
     end
     @discussion = Discussion.new
-    @discussions.uniq #TODO : Need to be tested
+    @discussions = discussions.uniq
   end
 
   def show
