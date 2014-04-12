a3c4cad5414959da04ab4cdd3533e3285a26fd06
Marco Aeberli <marco.aeberli@he-arc.ch>
2014-04-12 13:36:48 +0200

    FIX: Discussion creation from user sidebar and from promotions user list fix #51

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -10,7 +10,7 @@ class DiscussionsController < ApplicationController
       discussions += promotion.establishment.discussions
     end
     current_user.accepted_followed_promotions.each do |promotion|
-        discussions += promotion.discussions
+      discussions += promotion.discussions
     end
     @discussion = Discussion.new
     @discussions = discussions.uniq
@@ -39,20 +39,25 @@ class DiscussionsController < ApplicationController
 
   def create
     user_ids = params[:discussion][:user_ids]
-    if user_ids.size == 0
-      redirect_to :back
-    elsif user_ids.size == 1 && user_ids.contains?(current_user.id)
-      redirect_to :back
+
+    # Remove empty string element (provided by and necessary for choosen)
+    user_ids.delete("")
+
+    # convert array elements from string to integers
+    user_ids_int = user_ids.collect { |i| i.to_i }
+
+    if user_ids_int.size == 0 || (user_ids_int.size == 1 && user_ids_int.include?(current_user.id))
+      self.authorization_performed = true
+      redirect_to :back and return
     end
 
-    user_ids.delete("")
-    user_ids_int = user_ids.collect{|i| i.to_i}
+    # append current user to the discussion
     user_ids_int.append(current_user.id).uniq!
 
     discussion_new = Discussion.new
     discussion_new.users = User.find(user_ids_int)
 
-    d = Discussion.already_exists(discussion_new.users,[],[])
+    d = Discussion.already_exists(discussion_new.users, [], [])
     if d.size == 1
       d = d[0]
       authorize_action_for(d)
