af4f9c2c006fd38759a0064102d0748ac8ae1635
Marco Aeberli <marco.aeberli@he-arc.ch>
2014-04-12 11:35:10 +0200

    ADD: Completed search fonctionality (refactoring and :lipstick:)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,11 @@ class SearchController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @promotions = Promotion.search(params[:search], current_user.general_establishment)
-    @users = User.search(params[:search], current_user.general_establishment)
+    keyword = params[:keyword]
+    @search_active = keyword != nil
+    if @search_active
+      @promotions = Promotion.search(params[:keyword], current_user.general_establishment)
+      @users = User.search(params[:keyword], current_user.general_establishment)
+    end
   end
 end
